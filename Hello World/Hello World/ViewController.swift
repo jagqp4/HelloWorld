5f8dd5ea7e23d59dc7d3332c0c6e27ca4341ac48
Jordan Gunter <jordangunter@Jordans-MacBook-Pro.local>
2017-06-05 16:11:06 -0500

    created two buttons which change the text content of a label

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+   
+    @IBOutlet weak var helloButton: UIButton!
+    
+    @IBOutlet weak var clearButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Mark: Actions
+    @IBAction func setLabelText(_ sender: UIButton) {
+        welcomeLabel.text = "Hello World!"
+    }
+    
+    @IBAction func clearLabelText(_ sender: UIButton) {
+        welcomeLabel.text = ""
+    }
+    
+    
 }
 
